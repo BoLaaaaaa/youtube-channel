d3d68768e0edd0ad495c98b000c298a5503f17ee
pola <bolapop40@gmail.com>
2024-01-03 00:23:42 +0200

    youtuby

--- js/log.js
+++ js/log.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
-    // حذف الفيديو من سجل المشاهدة عندما يُضغط على إشارة الخطأ الموجودة في الزاوية
+$(document).ready(function () {
     $(".remove").click(function () {
-        $(this).parents('.card').hide(); 
+        $(this).parents(".card").hide();
     });
 });
--- js/watch.js
+++ js/watch.js
@@ -1,27 +1,24 @@
 $(document).ready(function () {
-    /*عند الضغط على الزر استدعي الدالة لتعمل وتظهر وتخفي النص*/
-    $("#myBtn").on("click", function(){
+    $("#myBtn").on("click", function () {
         read();
     });
-    /*إظهار وإخفاء الردود*/
-    $(".reply").click(function() {
+    $(".reply").click(function () {
         $(this).parents("div.row").next("div.card").toggle();
     });
 });
 
-/*الدالة التي نستخدمها لإظهار وإخفاء العناصر*/
 function read() {
-    var dots = document.getElementById("dots");/*أحضر النقاط الثلاث*/
-    var moreText = document.getElementById("more");/*أحضر النص الذي نريد إظهاره وإخفائه*/
-    var btnText = document.getElementById("myBtn");/*أحضر الزر الذي سنضغط عليه*/
-  
-    if (dots.style.display === "none") {/*إذا كانوا النقاط الثلاث مخفيين يعني النص ظاهر وضغطت عالزر*/
-      dots.style.display = "inline";/*أظهر النقط */
-      btnText.innerHTML = "عرض المزيد";
-      moreText.style.display = "none";/*أخفي النص */
-    } else {/*إذا كانت النقط الثلاث ظاهرين أي النص مخفي وضغطت على الزر */
-      dots.style.display = "none";/*أخفي النقط */
-      btnText.innerHTML = "عرض عناصر أقل";
-      moreText.style.display = "inline";/*أظهر النص المخفي */
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "عرض المزيد";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "عرض عناصر أقل";
+        moreText.style.display = "inline";
     }
 }
